3270de85fd272aa2f321309bfbd5012ed5e64ea3
Revi Permana <revipermana231@gmail.com>
2021-09-22 17:39:53 +0700

    12

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+
+        $title = '';
+
+        if(request('category')){
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        if(request('author')){
+            $author = User::firstWhere('username', request('author'));
+            $title = ' By ' . $author->name;
+        }
+
         return view('posts', [
-            'title' => 'Posts',
+            'title' => 'Posts' . $title,
             "active" => 'posts',
-            // 'posts' => Post::all()
-        'posts' => Post::latest()->get()
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
     public function show(Post $post)
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,8 +2,24 @@
 
 @section('container')
 
-    <h1 class="mb-5">{{ $title }}</h1>
+    <h1 class="mb-3 text-center">{{ $title }}</h1>
 
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Pencarian" name="search" value="{{ request('search') }}">
+                    <button class="btn btn-primary" type="submit">Cari</button>
+                  </div>
+            </form>
+        </div>
+    </div>
     @if ($posts->count())
 
     <div class="card mb-3">
@@ -11,8 +27,8 @@
         <div class="card-body text-center">
           <h5 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
           <p><small class="text-muted">
-              By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}
-            </a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none"> {{ $posts[0]->category->name }}
+              By. <a href="/posts?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}
+            </a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none"> {{ $posts[0]->category->name }}
             </a>{{ $posts[0]->created_at->diffForHumans() }}
           </small>
         </p>
@@ -23,23 +39,20 @@
         </div>
       </div>
 
-      @else
-      <p class="text-center fs-4">No post found.</p>
 
-    @endif
 
       <div class="container">
           <div class="row">
             @foreach ($posts->skip(1) as $post)
               <div class="col-4 mb-3">
                 <div class="card">
-                    <div class="position-absolute  px-3 py-2 text-white" style="background-color: rgba(0,0,0,0.7);"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+                    <div class="position-absolute  px-3 py-2 text-white" style="background-color: rgba(0,0,0,0.7);"><a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
                     <img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}" class="card-img-top img-fluid" alt="{{ $post->category->name }}">
                     <div class="card-body">
                       <h5 class="card-title">{{ $post->title }}</h5>
                       <p>
                         <small class="text-muted">
-                        By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}
+                        By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}
                       </a>{{ $post->created_at->diffForHumans() }}
                     </small>
                   </p>
@@ -50,7 +63,14 @@
               </div>
               @endforeach
           </div>
-
       </div>
+      @else
+      <p class="text-center fs-4">No post found.</p>
+    @endif
 
+    <div class="d-flex justify-content-end">
+
+    {{ $posts->links() }}
+
+</div>
 @endsection
