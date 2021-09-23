b44885e3c6da293ea4f109de35556ae652cef851
Revi Permana <revipermana231@gmail.com>
2021-09-23 17:45:16 +0700

    14

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,24 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required|max:25',
+            'username' => ['required', 'min:3', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:6|max:20'
+        ]);
+
+        // $validate['password'] = bcrypt($validate['password']);
+
+        $validate['password'] = Hash::make($validate['password']);
+
+        User::create($validate);
+
+        // $request->session()->flash('success', 'Registration Successful! Silahkan Login.');
+
+        return redirect('/login')->with('success', 'Registration Successful! Silahkan Login.');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -5,24 +5,45 @@
 <div class="row justify-content-center">
     <div class="col-lg-5">
         <main class="form-registration">
-            <form>
+            <form action="/register" method="post">
+                @csrf
               <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
 
               <div class="form-floating">
-                <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="name">
+                <input type="text" name="name" class="form-control rounded-top @error('name')is-invalid @enderror" id="name" placeholder="name" required value="{{ old('name') }}">
                 <label for="name">Nama</label>
+                @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                  @enderror
               </div>
               <div class="form-floating">
-                <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                <input type="text" name="username" class="form-control @error('username')is-invalid @enderror" id="username" placeholder="Username" required value="{{ old('username') }}">
                 <label for="username">Username</label>
+                @error('username')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                  @enderror
               </div>
               <div class="form-floating">
-                <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+                <input type="email" class="form-control @error('email')is-invalid @enderror" name="email" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
                 <label for="email">Email address</label>
+                @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                  @enderror
               </div>
               <div class="form-floating">
-                <input type="password" class="form-control rounded-bottom" name="password" id="password" placeholder="Password">
+                <input type="password" class="form-control rounded-bottom @error('password')is-invalid @enderror" name="password" id="password" placeholder="Password" required>
                 <label for="password">Password</label>
+                @error('password')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                  @enderror
               </div>
               <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">Sign in</button>
             </form>
