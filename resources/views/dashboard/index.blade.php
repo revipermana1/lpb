d63cba69d5e233b1f8e76628f8d0909442b2319d
Revi Permana <revipermana231@gmail.com>
2021-09-24 18:48:21 +0700

    15

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome, Revi Permana</h1>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,10 +20,27 @@
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
+                    @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Welcome back, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li>
+                              <form action="/logout" method="POST">
+                                  @csrf
+                                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                              </form>
+                        </ul>
+                      </li>
+                    @else
             <li class="nav-item">
                 <a href="/login" class="nav-link {{ ($active === 'login' ? 'active' : '') }} "><i class="bi bi-box-arrow-in-right"></i> Sign In</a>
             </li>
         </ul>
+        @endauth
       </div>
     </div>
 </nav>
